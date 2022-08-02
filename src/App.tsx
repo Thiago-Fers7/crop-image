f4f5b10f458512227f7f128dc7ba6872874f845b
Luiz Thiago <thiago_fers7@hotmail.com>
2022-08-02 01:43:53 -0300

    Dinamic box resizing

--- src/App.tsx
+++ src/App.tsx
@@ -6,32 +6,58 @@ import Layout from './Components/HTML/Layout';
 import './assets/styles/global.css';
 import styles from './app.module.css';
 
-type holdingValues = { isHolding: boolean; buttonRef: string };
+type holdingValues = { isHolding: boolean; buttonRef: 'top-left' | 'bottom-right' | null };
 
 export default function App() {
   const [imgValue, setImgValue] = useState<FileList | null>(null);
   const [imgSrc, setImgSrc] = useState('');
 
-  const holdingMouse = useRef({} as holdingValues);
-
   const editContainerRef = useRef<HTMLDivElement>(null);
-  const resizeBarLeftRef = useRef<HTMLButtonElement>(null);
-  const resizeBarRightRef = useRef<HTMLButtonElement>(null);
+  const resizerBox = useRef<HTMLDivElement>(null);
+  const resizeBarTopLeftRef = useRef<HTMLButtonElement>(null);
+  const resizeBarBottomRightRef = useRef<HTMLButtonElement>(null);
   const resizeVaues = useRef({ positionButtonLeft: 0, positionButtonRight: 0, distanceBeetweenButtons: 0 });
 
-  function handleHolding({ isHolding, buttonRef }: holdingValues) {
-    holdingMouse.current = { isHolding, buttonRef };
+  const [holding, setHolding] = useState<holdingValues>({ isHolding: false, buttonRef: null });
 
-    if (isHolding) {
-      editContainerRef.current && (editContainerRef.current.style.cursor = 'e-resize');
-    } else {
-      editContainerRef.current && (editContainerRef.current.style.cursor = 'default');
+  useEffect(() => {
+    function handleResizeSquare(e: any) {
+      const { offsetLeft, offsetHeight, offsetWidth, offsetTop } = editContainerRef.current?.parentNode as HTMLElement;
+      const cursorPositionX = e.clientX;
+      const cursorPositionY = e.clientY;
+
+      const moveLeft = cursorPositionX - offsetLeft;
+      const moveTop = cursorPositionY - offsetTop;
+
+      if (resizerBox.current) {
+        if (holding.buttonRef === 'top-left') {
+          resizerBox.current.style.left = `${moveLeft}px`;
+          resizerBox.current.style.top = `${moveTop}px`;
+          return;
+        }
+
+        if (holding.buttonRef === 'bottom-right') {
+          resizerBox.current.style.right = `${(offsetLeft + offsetWidth) - cursorPositionX}px`;
+          resizerBox.current.style.bottom = `${(offsetTop + offsetHeight) - cursorPositionY}px`;
+          return;
+        }
+      }
     }
-  }
+
+    const element = editContainerRef.current;
+
+    if (holding.isHolding) {
+      element?.addEventListener('mousemove', handleResizeSquare);
+    }
+
+    return () => {
+      element?.removeEventListener('mousemove', handleResizeSquare);
+    }
+  }, [holding]);
 
   useEffect(() => {
     function handleMouseUp() {
-      handleHolding({ isHolding: false, buttonRef: '' });
+      setHolding({ isHolding: false, buttonRef: null });
     }
 
     if (imgValue) {
@@ -59,54 +85,6 @@ export default function App() {
     setImgSrc(src);
   }, [imgValue]);
 
-  function handleMoveBar(e: MouseEvent) {
-    if (!holdingMouse.current.isHolding) return;
-
-    function moveBar(barPosition: number) {
-      if (resizeBarLeftRef.current && resizeBarRightRef.current) {
-        if (holdingMouse.current.buttonRef === 'left') {
-          if (barPosition >= resizeVaues.current.positionButtonRight) {
-            resizeBarLeftRef.current.style.left = `${resizeVaues.current.positionButtonRight}px`;
-          } else {
-            resizeBarLeftRef.current.style.left = `${barPosition}px`;
-          }
-
-
-          resizeVaues.current.positionButtonLeft = barPosition;
-        } else {
-          if (barPosition <= resizeVaues.current.positionButtonLeft) {
-            resizeBarRightRef.current.style.left = `${resizeVaues.current.positionButtonLeft}px`;
-          } else {
-            resizeBarRightRef.current.style.left = `${barPosition}px`;
-          }
-
-          resizeVaues.current.positionButtonRight = barPosition;
-
-        }
-
-        resizeVaues.current.distanceBeetweenButtons = resizeVaues.current.positionButtonRight - resizeVaues.current.positionButtonLeft;
-      }
-    }
-
-    let { offsetLeft, offsetWidth } = editContainerRef.current?.parentNode as HTMLElement;
-
-    const cursorPosition = e.clientX;
-    const maxPosition = offsetLeft + offsetWidth;
-
-    if (cursorPosition >= maxPosition) {
-      moveBar(offsetWidth);
-      return;
-    }
-
-    if (cursorPosition <= offsetLeft) {
-      moveBar(0);
-      return;
-    }
-
-    const freePosition = cursorPosition - offsetLeft;
-    moveBar(freePosition);
-  }
-
   return (
     <Layout>
       <main className={`${styles['main-container']}`}>
@@ -120,24 +98,24 @@ export default function App() {
 
         <section>
           {imgSrc && (
-            <div className={`${styles['img-edit-container']} ${styles['expand']}`} ref={editContainerRef} onMouseMove={handleMoveBar}>
+            <div className={`${styles['img-edit-container']} ${styles['expand']}`} ref={editContainerRef} >
               <img src={imgSrc} alt="Imagem" className={`${styles['original-img']}`} />
 
-              <div className={styles['resizer-box']} />
+              <div className={styles['resizer-box']} ref={resizerBox}>
+                <button
+                  type="button"
+                  className={`${styles['resizer-bar']} ${styles['left']}`}
+                  onMouseDown={() => setHolding({ isHolding: true, buttonRef: 'top-left' })}
+                  ref={resizeBarTopLeftRef}
+                />
 
-              <button
-                type="button"
-                className={`${styles['resizer-bar']} ${styles['left']}`}
-                onMouseDown={() => handleHolding({ isHolding: true, buttonRef: 'left' })}
-                ref={resizeBarLeftRef}
-              />
-
-              <button
-                type="button"
-                className={`${styles['resizer-bar']} ${styles['right']}`}
-                ref={resizeBarRightRef}
-                onMouseDown={() => handleHolding({ isHolding: true, buttonRef: 'right' })}
-              />
+                <button
+                  type="button"
+                  className={`${styles['resizer-bar']} ${styles['right']}`}
+                  ref={resizeBarBottomRightRef}
+                  onMouseDown={() => setHolding({ isHolding: true, buttonRef: 'bottom-right' })}
+                />
+              </div>
             </div>
           )}
         </section>
